84a1b3459abc85e808c9501eae570d55a14f484b
VolodymyrVi <sqlphpjs@gmail.com>
2021-10-03 22:28:49 +0300

    added an image upload form for 'preview_image' and 'main_image'

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -27,17 +27,49 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{ route('admin.post.store') }}" method="POST" class="w-25">
+                        <form action="{{ route('admin.post.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="title" placeholder="Назва поста">
+                            <div class="form-group w-25">
+                                <input type="text" class="form-control" name="title" placeholder="Назва поста"
+                                value="{{ old('title') }}">
                                 @error('title')
                                     <div class="text-danger">
                                         Це поле необхідно заповнити
                                     </div>
                                 @enderror
                             </div>
-                            <input type="submit" class="btn btn-primary" value="Додати">
+                            <div class="form-group">
+                                <textarea name="content" id="summernote">
+                                    {{ old('content') }}
+                                </textarea>
+                                @error('content')
+                                <div class="text-danger">
+                                    Це поле необхідно заповнити
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Додати превью зображення</label>
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="preview_image">
+                                    <label class="custom-file-label">Обрати файл</label>
+                                </div>
+
+                            </div>
+
+                            <div class="form-group w-50">
+                                <label>Додати головне зображення</label>
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="main_image">
+                                    <label class="custom-file-label">Обрати файл</label>
+                                </div>
+
+                            </div>
+
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Додати">
+                            </div>
+
 
                         </form>
                     </div>
