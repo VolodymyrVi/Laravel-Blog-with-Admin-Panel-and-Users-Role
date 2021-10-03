99eef29de485002a5ec543e02052bc98f4545ff5
VolodymyrVi <sqlphpjs@gmail.com>
2021-10-03 20:57:32 +0300

    added custon toolbar for summernote

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -79,8 +79,16 @@
 <!-- AdminLTE App -->
 <script src="{{ asset('dist/js/adminlte.js') }}"></script>
 <script>
-    $(document).ready(function() {
-        $('#summernote').summernote();
+    $('#summernote').summernote({
+        toolbar: [
+            // [groupName, [list of button]]
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+        ]
     });
 </script>
 </body>
