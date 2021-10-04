15ee7e2efff52604305507f2367a6df990737c8e
VolodymyrVi <sqlphpjs@gmail.com>
2021-10-05 02:33:28 +0300

    added edit form

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -27,19 +27,85 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{ route('admin.post.update', $post->id) }}" method="POST" class="w-25">
+                        <form action="{{ route('admin.post.update', $post->id) }}" method="POST" class="" enctype="multipart/form-data">
                             @csrf
                             @method('PATCH')
-                            <div class="form-group">
+                            <div class="form-group w-50">
                                 <input type="text" class="form-control" name="title" placeholder="Назва поста"
-                                value="{{ $post->title }}">
+                                       value="{{ $post->title }}">
                                 @error('title')
-                                    <div class="text-danger">
-                                        Це поле необхідно заповнити
-                                    </div>
+                                <div class="text-danger">
+                                    Це поле необхідно заповнити
+                                </div>
                                 @enderror
                             </div>
-                            <input type="submit" class="btn btn-primary" value="Оновити">
+                            <div class="form-group">
+                                <textarea name="content" id="summernote">
+                                    {{ $post->content }}
+                                </textarea>
+                                @error('content')
+                                <div class="text-danger">
+                                    Це поле необхідно заповнити
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Додати превью зображення</label>
+                                <div class="w-25">
+                                    <img src="{{ url('storage/' . $post->preview_image) }}" alt="preview_image" class="w-50">
+                                </div>
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="preview_image">
+                                    <label class="custom-file-label">Обрати файл</label>
+                                </div>
+                                @error('preview_image')
+                                <div class="text-danger">
+                                    Це поле необхідно заповнити
+                                </div>
+                                @enderror
+
+                            </div>
+
+                            <div class="form-group w-50">
+                                <label>Додати головне зображення</label>
+                                <div class="w-25">
+                                    <img src="{{ url('storage/' . $post->main_image) }}" alt="main_image" class="w-50">
+                                </div>
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="main_image">
+                                    <label class="custom-file-label">Обрати файл</label>
+                                </div>
+                                @error('main_image')
+                                <div class="text-danger">
+                                    Це поле необхідно заповнити
+                                </div>
+                                @enderror
+
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="">Оберіть категорію</label>
+                                <select class="form-control" name="category_id" id="">
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $category->id == $post->category_id ? ' selected' : '' }}
+                                        >{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <lavel>Теги</lavel>
+                                <select class="select2" multiple="multiple" data-placeholder="Обрати теги" style="width: 100%" name="tag_ids[]" id="">
+                                    @foreach($tags as $tag)
+                                        <option {{ is_array( $post->tags->pluck('id')->toArray() ) && in_array($tag->id, $post->tags->pluck('id')->toArray() ) ? ' selected' : '' }} value="{{ $tag->id }}">{{ $tag->title }}</option>
+                                    @endforeach
+                                </select>
+
+                            </div>
+
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Оновити">
+                            </div>
 
                         </form>
                     </div>
