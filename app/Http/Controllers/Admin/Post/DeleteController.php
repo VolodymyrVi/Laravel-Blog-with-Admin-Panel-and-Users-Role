97e0534dc3a0f06a22f371caa06b58403ffc5a70
VolodymyrVi <sqlphpjs@gmail.com>
2021-10-05 03:32:23 +0300

    added SoftDeletes for posts

--- app/Http/Controllers/Admin/Post/DeleteController.php
+++ app/Http/Controllers/Admin/Post/DeleteController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Post\UpdateRequest;
 use App\Models\Post;
 
 class DeleteController extends Controller
