35f6cad5a3b4c6a87a9bb9bc98196ec194eb2b66
VolodymyrVi <sqlphpjs@gmail.com>
2021-10-06 18:10:08 +0300

    added CRUD for users

---
+++ app/Http/Requests/Admin/User/UpdateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Admin\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => 'Це поле обовязкове до заповнення',
+            'name.string' => 'Імя має бути рядкового типу',
+            'email.required' => 'Це поле обовязкове до заповнення',
+            'email.string' => 'Пошта має відповідати рядковому типу',
+            'email.email' => 'Ваша пошта повинна відповідати наступному формату mail.example.com',
+            'email.unique' => 'Користувач з таким email вже існує',
+            'password.required' => 'Це поле обовязкове до заповнення',
+            'password.string' => 'Пароль має бути рядкового типу'
+        ];
+    }
+}
