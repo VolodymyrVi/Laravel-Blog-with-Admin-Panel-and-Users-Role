c5d0bb3ed6a3ef46a2f341b8316712a4a08cd151
VolodymyrVi <sqlphpjs@gmail.com>
2021-10-07 11:12:12 +0300

    added middleware - verified

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::group(['namespace'  => 'Main'], function(){
     Route::get('/', 'IndexController');
 });
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth' ,'admin']], function (){
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth' ,'admin', 'verified']], function (){
     Route::group(['namespace' => 'Main'], function (){
         Route::get('/', 'IndexController');
     });
@@ -63,9 +63,5 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['aut
     });
 });
 
+Auth::routes(['verify' => true]);
 
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
